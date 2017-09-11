0070405d0111686ab1c0dc0e799f397612fd12dc
Ryan Cranfill <ryan.cranfill@datascopeanalytics.com>
2017-09-11 11:40:28 -0500

    add some stuff

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# cena
+# Project Cena
+## Setup
+- Install OpenCV
+    - [Full instructions here](http://www.pyimagesearch.com/2016/12/19/install-opencv-3-on-macos-with-homebrew-the-easy-way/)
+    - Basically:
+        - `brew tap homebrew/science`
+        - `brew install opencv3 --with-contrib --with-python3 --HEAD`
+- `pip install -r requirements.txt`
---
+++ cena/face_trainer.py
@@ -0,0 +1,20 @@
+import glob
+import cv2
+import numpy as np
+
+
+path = 'dataSet/*'
+detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+imgs = []
+labels = []
+
+for f in glob.glob(path):
+    i = cv2.imread(f, cv2.IMREAD_GRAYSCALE)
+    i_resize = cv2.resize(i,(224,224))
+    imgs.append(i_resize)
+    labels.append(int(f.split('.')[1]))
+
+recognizer.train(imgs,np.array(labels))
+recognizer.write('trained_recognizer.yaml')
